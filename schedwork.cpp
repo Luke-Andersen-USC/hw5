3b25dcecc67e782c2c67489d4da0b2af76de53a5
Luke Andersen <lwanders@usc.edu>
2023-04-11 01:40:51 +0000

    commiting hw5!

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,10 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
-
+bool scheduleHelper(int row, int col,
+std::map<int,int>& original, 
+AvailabilityMatrix& avail, 
+DailySchedule& sched, size_t dailyNeed);
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -37,8 +40,83 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    std::map<int,int> workers;
+		for(unsigned int i = 0; i < avail[0].size(); i++)
+		{
+			workers.insert(std::make_pair<int,int>(i,maxShifts));
+		}
 
+		std::vector<Worker_T> empty;
+		sched.push_back(empty);
+		AvailabilityMatrix availCopy = avail;
 
+		bool works = scheduleHelper(0, 0, workers, availCopy, sched, dailyNeed);
+
+		return works;
 
 }
 
+bool scheduleHelper(int row, int col, 
+std::map<int,int>& original, 
+AvailabilityMatrix& avail, 
+DailySchedule& sched, size_t dailyNeed)
+{
+		//everything true!
+    if((unsigned)row == avail.size())
+    {
+				if(!sched.empty())
+				{
+					sched.pop_back();
+				}
+        return true;
+    }
+
+		if(sched[row].size() == dailyNeed)
+		{
+			
+			std::vector<Worker_T> empty;
+			sched.push_back(empty);
+			
+			bool next = scheduleHelper(row + 1, 0, original, avail, sched, dailyNeed);
+
+			if(next)
+			{
+				return next;
+			}
+			sched.pop_back();
+		}
+    else 
+		{
+
+			for(std::map<int,int>::iterator it = original.begin(); it != original.end(); it++)
+			{		
+			
+				if(avail[row][col] == true && it->second > 0)
+				{
+					sched[row].push_back(it->first);
+					original[it->first] -= 1;
+
+					avail[row][col] = 0;
+
+					bool next = true;
+					
+					next = scheduleHelper(row, 0, original, avail, sched, dailyNeed);
+					
+					if(next)
+					{
+						return next;
+					}
+
+					avail[row][col] = 1;
+					original[it->first]++;
+					sched[row].pop_back();
+
+				}
+
+				col++;
+			}
+			
+		}
+
+		return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordMaker(set<std::string>& words, string word, string floating, int place);
+void wordRefiner(std::set<std::string>& refinedWords, string word, int place, const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -23,6 +24,98 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+		set<std::string> words;
+		set<std::string> refinedWords;
+		string floatingAdd = floating;		
+
+		int fSize = floating.size();
+		int emptyCount = 0;
+		for(unsigned int i = 0; i < in.size(); i++)
+		{
+
+			if(in[i] == '-')
+			{
+				if(emptyCount >= fSize)
+				{
+					floatingAdd += '*';
+				}
+				else
+				{
+					emptyCount++;
+				}
+			}
+		}
+
+		wordMaker(words, in, floatingAdd, 0);
+
+		for(set<std::string>::iterator it = words.begin(); it != words.end(); it++)
+		{
+			//cout << "this loop!" << endl;
+			wordRefiner(refinedWords, *it, 0, dict);
+		}
+
+		return refinedWords;
+
+
 }
 
 // Define any helper functions here
+
+void wordMaker(std::set<std::string>& words, string word, string floating, int place)
+{
+	if(word.length() == (unsigned)place)
+	{
+		words.insert(word);
+	}
+	else
+	{
+		//adding every other yellow character
+		if(word[place] == '-')
+		{
+			for(unsigned int i = 0; i < floating.size(); i++)
+			{
+				string floatingFixed = floating;
+				floatingFixed.erase(i, 1);
+
+				word[place] = floating[i];
+				wordMaker(words, word, floatingFixed, place + 1);
+			}
+		}
+		else
+		{
+			wordMaker(words, word, floating, place + 1);
+		}
+
+	}
+
+}
+
+//cleans up leftover spaces
+void wordRefiner(std::set<std::string>& refinedWords, string word, int place, const std::set<std::string>& dict)
+{
+	if(word.length() == (unsigned)place)
+	{
+		if(dict.find(word) != dict.end())
+		{
+			refinedWords.insert(word);
+		}
+	}
+	else
+	{
+		
+		if(word[place] == '*')
+		{
+			for(char letter = 'a'; letter <= 'z'; letter++)
+			{
+				word[place] = letter;
+				wordRefiner(refinedWords, word, place + 1, dict);
+			}
+		}
+		else
+		{
+			wordRefiner(refinedWords, word, place + 1, dict);
+		}
+
+
+	}
+}
